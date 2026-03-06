644849315eed64d557bc893fa40be4cc46a3d0bf
mdimranhossain78 <mdimrahossan146@gmail.com>
2026-03-07 00:05:07 +0600

    completed login form

---
+++ script/login.js
@@ -0,0 +1,19 @@
+
+const loginBtn = document.getElementById('login-btn')
+loginBtn.addEventListener('click', function(){
+//   get input user name
+const userName = document.getElementById('user-name')
+ const nameValue = userName.value ;
+ const logingPassword = document.getElementById('loging-password');
+ const passValue = logingPassword.value;
+ 
+
+ if(nameValue === 'admin' && passValue === 'admin123'){
+    alert('success')
+     window.location.assign("/home.html")
+ }
+ else{
+    alert('feiled try agine')
+ }
+
+})
